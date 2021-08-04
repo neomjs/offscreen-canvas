641c14ac89e3bc96ceb443230e1478239e25e683
tobiu <tobiasuhlig78@gmail.com>
2021-08-04 19:49:16 +0200

    canvas.Helper: importing d3 dependencies

--- apps/myapp/canvas/Helper.mjs
+++ apps/myapp/canvas/Helper.mjs
@@ -1,5 +1,20 @@
 import Base from '../../../node_modules/neo.mjs/src/core/Base.mjs';
 
+import '../../../node_modules/d3-array/dist/d3-array.js';
+import '../../../node_modules/d3-color/dist/d3-color.js';
+import '../../../node_modules/d3-format/dist/d3-format.js';
+import '../../../node_modules/d3-interpolate/dist/d3-interpolate.js';
+import '../../../node_modules/d3-scale-chromatic/dist/d3-scale-chromatic.js';
+import '../../../node_modules/d3-random/dist/d3-random.js';
+import '../../../node_modules/d3-scale/dist/d3-scale.js';
+import '../../../node_modules/d3-shape/dist/d3-shape.js';
+import '../../../node_modules/d3-time-format/dist/d3-time-format.js';
+import '../../../node_modules/@d3fc/d3fc-extent/build/d3fc-extent.js';
+import '../../../node_modules/@d3fc/d3fc-random-data/build/d3fc-random-data.js';
+import '../../../node_modules/@d3fc/d3fc-rebind/build/d3fc-rebind.js';
+import '../../../node_modules/@d3fc/d3fc-series/build/d3fc-series.js';
+import '../../../node_modules/@d3fc/d3fc-webgl/build/d3fc-webgl.js';
+
 /**
  * @class MyApp.canvas.Helper
  * @extends Neo.core.Base
