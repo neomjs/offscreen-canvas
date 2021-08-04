082a144c81b443ad5aa534119fcaf5d563ed174c
tobiu <tobiasuhlig78@gmail.com>
2021-08-04 19:28:43 +0200

    MyApp.view.WebGlComponent: base class

---
+++ apps/myapp/view/WebGlComponent.mjs
@@ -0,0 +1,27 @@
+import Canvas from '../../../node_modules/neo.mjs/src/component/Canvas.mjs';
+
+/**
+ * @class MyApp.view.WebGlComponent
+ * @extends Neo.component.Canvas
+ */
+class WebGlComponent extends Canvas {
+    static getConfig() {return {
+        className: 'MyApp.view.WebGlComponent'
+    }}
+
+    /**
+     * Triggered after the offscreenRegistered config got changed
+     * @param {Boolean} value
+     * @param {Boolean} oldValue
+     * @protected
+     */
+    afterSetOffscreenRegistered(value, oldValue) {
+        super.afterSetMounted(value, oldValue);
+
+        console.log('afterSetOffscreenRegistered');
+    }
+}
+
+Neo.applyClassConfig(WebGlComponent);
+
+export {WebGlComponent as default};
