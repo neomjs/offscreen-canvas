d9070ab2c4b6845ca6038f4830affb5619085826
tobiu <tobiasuhlig78@gmail.com>
2025-06-03 15:54:00 +0200

    top-level service worker

---
+++ ServiceWorker.mjs
@@ -0,0 +1,35 @@
+import Neo         from './node_modules/neo.mjs/src/Neo.mjs';
+import * as core   from './node_modules/neo.mjs/src/core/_export.mjs';
+import ServiceBase from './node_modules/neo.mjs/src/worker/ServiceBase.mjs';
+
+/**
+ * @class Neo.ServiceWorker
+ * @extends Neo.worker.ServiceBase
+ * @singleton
+ */
+class ServiceWorker extends ServiceBase {
+    static config = {
+        /**
+         * @member {String} className='Neo.ServiceWorker'
+         * @protected
+         */
+        className: 'Neo.ServiceWorker',
+        /**
+         * @member {Boolean} singleton=true
+         * @protected
+         */
+        singleton: true,
+        /**
+         * @member {String} version='9.9.0'
+         */
+        version: '9.9.0'
+    }
+
+    /**
+     * @member {String} workerId='service'
+     * @protected
+     */
+    workerId = 'service'
+}
+
+export default Neo.setupClass(ServiceWorker);
